45c68af4f7d595936ac93b69ce352f0b040d85c7
Aaron Zuspan <aa.zuspan@gmail.com>
2023-02-27 19:54:29 -0800

    Remove leftover comments

--- components/theme_toggle.jsx
+++ components/theme_toggle.jsx
@@ -3,7 +3,6 @@ import { ThemeContext } from "./theme";
 import styles from "../styles/theme_toggle.module.css";
 
 const changeTheme = (theme) => {
-  // Set the dark of light class on the body
   document.body.classList.remove(theme === "dark" ? "light" : "dark");
   document.body.classList.add(theme);
 };
@@ -11,7 +10,6 @@ const changeTheme = (theme) => {
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  // TODO: Set a default theme, load from local storage, browser default, etc
   useEffect(() => {
     changeTheme(theme);
   }, [theme]);
