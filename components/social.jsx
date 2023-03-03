42cdea730024d494d62e067d7cffeced328584cc
Aaron Zuspan <50475791+aazuspan@users.noreply.github.com>
2023-03-02 22:10:45 -0800

    Custom social links (#10)

--- components/social.jsx
+++ components/social.jsx
@@ -1,20 +1,21 @@
-import { SocialIcon } from 'react-social-icons';
+import { FaTwitter, FaGithub, FaMastodon, FaLinkedin } from "react-icons/fa";
+import styles from "../styles/social.module.scss";
 
 export default function Social() {
   return (
-    <span>
-      <SocialIcon
-        url="https://twitter.com/aazuspan"
-      />
-      <SocialIcon
-        url="https://github.com/aazuspan"
-      />
-      <SocialIcon
-        url="https://fosstodon.org/@aazuspan"
-      />
-      <SocialIcon
-        url="https://www.linkedin.com/in/aaron-zuspan-91b5261b4"
-      />
+    <span className={styles["social-container"]}>
+      <a href="https://twitter.com/aazuspan" className={styles["social-link"]}>
+        <FaTwitter className={styles["social-button"]} />
+      </a>
+      <a href="https://github.com/aazuspan" className={styles["social-link"]}>
+        <FaGithub className={styles["social-button"]} />
+      </a>
+      <a href="https://fosstodon.org/@aazuspan" className={styles["social-link"]}>
+        <FaMastodon className={styles["social-button"]} />
+      </a>
+      <a href="https://www.linkedin.com/in/aaron-zuspan-91b5261b4" className={styles["social-link"]}>
+        <FaLinkedin className={styles["social-button"]} />
+      </a>
     </span>
   );
 }
