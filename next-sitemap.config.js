ccc841ab80714932a4ecd99cb09f195630e89f18
Aaron Zuspan <aa.zuspan@gmail.com>
2023-05-07 17:52:23 -0700

    Use canonical www domain in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://aazuspan.dev",
+  siteUrl: process.env.SITE_URL || "https://www.aazuspan.dev",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
