d4f037c1711e17b6eea32ff800e41402483ac27c
azuspan607 <aa.zuspan@gmail.com>
2023-11-02 21:59:54 -0700

    New paper!

--- pages/research.js
+++ pages/research.js
@@ -8,6 +8,12 @@ const RESEARCH = [
     items: [
       {
         title:
+          "Characterizing post-fire delayed tree mortality with remote sensing: Sizing up the elephant in the room",
+        href: "https://link.springer.com/article/10.1186/s42408-023-00223-1",
+        description: "Fire Ecology, 2023.",
+      },
+      {
+        title:
           "Cascadia Burning: The Historic, but not Historically Unprecedented, 2020 Wildfires in the Pacific Northwest, USA",
         href: "https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.4070",
         description: "Ecosphere, 2022.",
